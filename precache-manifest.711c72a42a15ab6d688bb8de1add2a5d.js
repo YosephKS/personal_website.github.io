4226ea58a4b1dd695f99dbc9db6a6dc90a1289f1
YosephKS <yosephkurniasoenggoro@gmail.com>
2019-10-14 16:20:27 +0800

    Updates

---
+++ precache-manifest.711c72a42a15ab6d688bb8de1add2a5d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb294f3235c864e2b8001b74c50334a",
+    "url": "/personal_website.github.io/index.html"
+  },
+  {
+    "revision": "9cc8ac8a51e116e8bab2",
+    "url": "/personal_website.github.io/static/css/2.d7a94725.chunk.css"
+  },
+  {
+    "revision": "0141c4021865aa468b33",
+    "url": "/personal_website.github.io/static/css/main.445e29d3.chunk.css"
+  },
+  {
+    "revision": "9cc8ac8a51e116e8bab2",
+    "url": "/personal_website.github.io/static/js/2.0c07d2a4.chunk.js"
+  },
+  {
+    "revision": "0141c4021865aa468b33",
+    "url": "/personal_website.github.io/static/js/main.c80a9826.chunk.js"
+  },
+  {
+    "revision": "b0407ad8a43a1e733271",
+    "url": "/personal_website.github.io/static/js/runtime-main.897d73ae.js"
+  },
+  {
+    "revision": "6e874c4537feb179f83e611a988d2fac",
+    "url": "/personal_website.github.io/static/media/Photo_YosephKS.6e874c45.jpg"
+  },
+  {
+    "revision": "7ad85b19203757b8c5e69cb5b566e83b",
+    "url": "/personal_website.github.io/static/media/logo-02.7ad85b19.png"
+  },
+  {
+    "revision": "021d8be117b38f68df5a0bc879380daa",
+    "url": "/personal_website.github.io/static/media/project2.021d8be1.png"
+  }
+]);
